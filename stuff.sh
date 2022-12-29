1bcc478bda6a8f20bc1c6a5cdebc438184655652
Earth <85964403+EarthRulerr@users.noreply.github.com>
2022-12-28 18:32:25 -0600

    Create stuff.sh

---
+++ stuff.sh
@@ -0,0 +1,8 @@
+git clone #repo link
+# Useful for allowing people to install a program on their computer. The link must be the main page of the repo.
+
+echo $PWD
+# Reports the file location stuff.
+
+echo $USER
+# Reports user.
